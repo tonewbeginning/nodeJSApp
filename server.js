843a4b8c598851b88fa96e02b68cca6a70b1ca1e
rs <tonewbeginning21@gmail.com>
2020-11-23 16:39:32 -0500

    trial nodejs code

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  fs.readFile('index.html',function(error, data){
+    if (error){
+      res.writeHead(404)
+      res.write('Error: File Not Found')
+    } else {
+      res.write(data)
+    }
+    res.end()
+  })
+});
+
+server.listen(port, function(error) {
+  if (error){
+    console.log(`Server running at http://${hostname}:${port}/`);
+  } else {
+    console.log('Server is listening on port ' + port)
+  }
+});
